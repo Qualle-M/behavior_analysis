a77a152c6babb58237581e29bb48b61635b5187b
Qualle-M <qualle9827@outlook.jp>
2025-04-15 12:03:12 +0900

    Update behavior_csv.R

--- behavior_csv.R
+++ behavior_csv.R
@@ -1,30 +1,30 @@
 # Apply 16 Arena at once
-raw_data_WT <- read.csv("E:/研究室/behavior/20250223_WT-Track Coordinates Report.csv")
+raw_data <- read.csv("your_filename.csv")
 
 # Divide data
-trimmed_WT <- raw_data_WT %>%
+trimmed_data <- raw_data %>%
   select(Trial.Arena, X.Coordinate, Y.Coordinate, Segment.Length, Sample.Time..Seconds.) %>%
   rename(Sample.Time = Sample.Time..Seconds.)
 
 # Apply functions
-trimmed_WT_result <- trimmed_WT %>%
+trimmed_result <- trimmed_data %>%
   group_by(Trial.Arena) %>%
   do(analyze.by.confidence(.)) %>%
   do(classified.distance(., threshold = 65))%>%
   arrange(as.numeric(parse_number(Trial.Arena)))
 
 # Create line plot
-arena_order <- unique(trimmed_WT_result$Trial.Arena)
-trimmed_WT_result$Trial.Arena <- factor(trimmed_WT_result$Trial.Arena, levels = arena_order)
+arena_order <- unique(trimmed_result$Trial.Arena)
+trimmed_result$Trial.Arena <- factor(trimmed_result$Trial.Arena, levels = arena_order)
 
-line_plot_WT <- ggplot(trimmed_WT_result, aes(x = Sample.Time, y = Cumsum.Distance, colour = Trial.Arena)) +
+line_plot <- ggplot(trimmed_result, aes(x = Sample.Time, y = Cumsum.Distance, colour = Trial.Arena)) +
   geom_line() +
-  labs(title = "Travel Distance (WT)", x = "Sample.Time", y = "Cumulative Distance") +
+  labs(title = "Travel Distance", x = "Sample.Time", y = "Cumulative Distance") +
   theme_minimal()
 line_plot_WT
 
 # Output final distance traveled
-WT_final <- trimmed_WT_result %>%
+final_distance <- trimmed_result %>%
   group_by(Trial.Arena) %>%
   summarise(Final.Distance = max(Cumsum.Distance, na.rm = T))%>%
   arrange(as.numeric(parse_number(Trial.Arena)))
